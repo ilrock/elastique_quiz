dbcf413de66e0c0f5a7b686782ed62bebaa656d8
Andrea <andrea.rocca3@gmail.com>
2018-09-21 17:15:34 +0200

    added basic questions ui

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,8 +1,31 @@
 <template>
     <v-container justify-center align-center grid-list-xs>
         <v-layout row wrap>
-            <v-flex xs12>
-                <h1> Welcome to Quiz </h1>
+            <v-flex xs12 sm8 offset-sm2>
+                <v-card>
+                  <v-toolbar card prominent class="elevation-2">
+                    <v-toolbar-title class="title"> Question? </v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <span> 30s </span>
+                  </v-toolbar>
+                  <v-card-text>
+                      <v-container grid-list-xs>
+                          <v-layout column>
+                              <v-flex xs12>
+                                  <div>
+                                      <v-btn outline color="indigo">Outline Button</v-btn>
+                                  </div>
+                                  <div>
+                                      <v-btn outline color="indigo">Outline Button</v-btn>
+                                  </div>
+                                  <div>
+                                      <v-btn outline color="indigo">Outline Button</v-btn>
+                                  </div>
+                              </v-flex>
+                          </v-layout>
+                      </v-container>
+                  </v-card-text>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
@@ -10,7 +33,11 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      text: ''
+    }
+  }
 }
 </script>
 
