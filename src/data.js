b7a0320e773e3f40c143791a1994c862b7b0c390
Andrea <andrea.rocca3@gmail.com>
2018-09-21 15:26:24 +0200

    added dummy data

---
+++ src/data.js
@@ -0,0 +1,38 @@
+const question = [
+  {
+    text: 'What color is the sky?',
+    answers: [
+      {
+        text: 'red',
+        correct: false
+      },
+      {
+        text: 'yellow',
+        correct: false
+      },
+      {
+        text: 'blue',
+        correct: true
+      }
+    ]
+  },
+  {
+    text: 'How many days in a year?',
+    answers: [
+      {
+        text: '300',
+        correct: false
+      },
+      {
+        text: '365',
+        correct: false
+      },
+      {
+        text: '366',
+        correct: true
+      }
+    ]
+  }
+]
+
+export { question }
