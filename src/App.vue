00252e54574dfbcd6dd385f6b048d1a9a7f5c97e
Andrea <andrea.rocca3@gmail.com>
2018-09-21 15:44:29 +0200

    added players form

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <v-app>
-    <router-view></router-view>
-  </v-app>
+    <v-app>
+        <v-toolbar color="primary">
+            
+        </v-toolbar>
+        <v-content>
+            <v-container grid-list-xs>
+                <router-view></router-view>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 <script>
 export default {
-  name: 'root'
+  name: 'app'
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,36 @@
 <template>
-    <h1> Welcome Home </h1>
+    <v-container justify-center align-center grid-list-xs>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-form>
+                    <v-text-field 
+                        v-for="(player, index) in players"
+                        v-model="player.name" 
+                        :key="index" 
+                        :label="`Player ${index} name`" 
+                        id="id">
+                    </v-text-field>
+                </v-form>
+                <v-btn color="success"> Start the quiz </v-btn>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      players: [
+        {
+          name: ''
+        },
+        {
+          name: ''
+        }
+      ]
+    }
+  }
 }
 </script>
 
